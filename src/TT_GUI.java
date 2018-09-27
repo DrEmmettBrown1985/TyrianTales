9d78a06943ca3915adb7c38c1bd613fbbb96a601
Arturo Negrette <arturoenegrette@gmail.com>
2018-09-27 13:55:48 -0400

    Added Fight Mechanism Logic

--- src/TT_GUI.java
+++ src/TT_GUI.java
@@ -1,26 +1,18 @@
-import javax.swing.JOptionPane;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-public class TT_GUI
-    {
-        public static void TT_GUI (String[]args)
+import java.awt.*;
+import java.io.*;
+import javax.swing.*;
+public class TT_GUI extends JFrame {
+
+
+
+
+    public static void main(String[] args) {
+        boolean troo = true;
+        while (troo = true)
         {
-            TT_Logic UserStats = new TT_Logic();
-            int UserHP = UserStats.HP;
-            int UserHL = UserStats.HL;
-            System.out.println("This is your current HP: "+UserHP);
+            break;
         }
+    }
 
-	
-
-	}
-
+}
 
--- src/TT_Logic.java
+++ src/TT_Logic.java
@@ -7,8 +7,12 @@
 //
 //
 //*************************************************************
+import javax.swing.*;
+import java.io.*;
 public class TT_Logic
 {
+
+
 
     public int EHP; //Enemy Health Level
     public int EDMG; //Enemy Damage
@@ -16,41 +20,96 @@ public class TT_Logic
     public int HP;  //User HP
     public int DMG; //User Damage
     public int HL;  //User Healing
+    public String logic;//Stuff thats output
+
 
     //Basic Enemies
-      public class GrnSlime
+    private class GrnSlime
+    {
+
+
+        int EHP = 5;
+        int EDMG = 3;
+        String Name = "Green Slime";
+
+
+    }
+
+    public class RedSlime
+    {
+        int EHP = 15;
+        int EDMG = 10;
+        String Name = "Red Slime";
+
+    }
+
+    public class BluSlime
+    {
+        int EHP = 30;
+        int EDMG = 15;
+        String Name = "Blue Slime";
+
+    }
+
+    public class KingSlime
+    {
+        int EHP = 50;
+        int EDMG = 30;
+        String Name = "King Slime";
+
+    }
+
+
+
+
+    public static void main(String[] args) throws InterruptedException
+    {
+
+
+        TT_Logic UserStats = new TT_Logic();
+        int UserHP = UserStats.HP;
+        int EnemyHP = 3;
+
+        int EnemyDMG = 3;
+        int UserDMG = UserStats.DMG;
+        boolean Test = true;
+
+
+        EnemyHP = 60;
+        UserHP = 90;
+
+        System.out.println("Current enemy name: ");
+        int cycles = 0;
+        while (Test = true)
         {
-            int EHP = 5;
-            int EDMG = 3;
-            String Name = "Green Slime";
+            if (EnemyHP>0)
+            {
+
+                UserHP -= EnemyDMG;
+                EnemyHP -= UserDMG;
+
+                cycles++;
+                String logic =("\nCurrent HP level " +UserHP +"\nCurrent Enemy HP level " +EnemyHP +"\nAttacks so far " +cycles);
+                System.out.println(logic);
+
+
+
+                Thread.sleep(1500);
+            }
+            else
+            {
+                EnemyHP = 60;
+            }
 
         }
 
-      public class RedSlime
-        {
-            int EHP = 15;
-            int EDMG = 10;
-            String Name = "Red Slime";
 
-        }
-      public class BluSlime
-        {
-            int EHP = 30;
-            int EDMG = 15;
-            String Name = "Blue Slime";
 
-        }
-       public class KingSlime
-        {
-           int EHP = 50;
-           int EDMG = 30;
-           String Name = "King Slime";
-
-        }
-
-    //Basic Characters
-
-      
-
+    }
 
 }
+
+
+
+
+
